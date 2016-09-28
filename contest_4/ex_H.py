e4949d2ef5172e03e01fef4a6b8a16317a679324
ZapisetskayaJulia <Juliaazap@mail.ru>
2016-09-28 19:00:46 +0300

    not finished

--- contest_4/ex_H.py
+++ contest_4/ex_H.py
@@ -1,5 +1,5 @@
 a = int(input())
-k = 1
+k = 2
 kmax = 1
 b = int(input())
 if a > b:
@@ -8,42 +8,45 @@ if a < b:
     z = 1
 if a == b:
     z = 2
-    k = 0
+    k = 1
+a = b
+b = int(input())
 while b != 0:
-    a = b
-    b = int (input())
+
     if z == 0:
         if a > b:
             z = 0
             k += 1
-        if k > kmax:
-            kmax = k
+            if k > kmax:
+                kmax = k
+        elif a < b:
+            z = 1
+            k = 2
+        elif a == b:
+            z = 2
+            k = 1
+    elif z == 1:
         if a < b:
             z = 1
-            k = 1
-        if a == b:
-            z = 2
-            k = 0
-    if z == 1:
-        if a < b:
-            z = 0
             k += 1
-        if k > kmax:
-            kmax = k
-        if a > b:
-            z = 1
-            k = 1
-        if a == b:
+            if k > kmax:
+                kmax = k
+        elif a > b:
+            z = 0
+            k = 2
+        elif a == b:
             z = 2
-            k = 0
-    if z == 2:
+            k = 1
+    elif z == 2:
         if a > b:
             z = 0
-            k = 1
-        if a < b:
+            k = 2
+        elif a < b:
             z = 1
+            k = 2
+        elif a == b:
+            z = 2
             k = 1
-        if a == b:
-            z = 0
-            k = 0
+    a = b
+    b = int(input())
 print (kmax)
